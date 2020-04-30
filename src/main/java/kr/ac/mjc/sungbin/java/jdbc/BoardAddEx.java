45e91ea785ef74f8cd161bc41282d2b800eb1f44
yang-sungbin <ysb05222@naver.com>
2020-05-01 00:57:22 +0900

    Update BoardAddEx.java

--- src/main/java/kr/ac/mjc/sungbin/java/jdbc/BoardAddEx.java
+++ src/main/java/kr/ac/mjc/sungbin/java/jdbc/BoardAddEx.java
@@ -5,9 +5,9 @@ public class BoardAddEx {
 	public static void main(String[] args) {
 		BoardDao boardDao = new BoardDao();
 		Board board = new Board();
-		board.setTitle("21대 총선 사전 투표율이 제도 도입 이래 최고치를 기록했다: 26.69%");
-		board.setContent("전국 4399만4247명 유권자 중 1174만2677명이 사전투표에 참여했다.");
-		board.setWriter("허핑턴포스트코리아");
+		board.setTitle("명지전문대");
+		board.setContent("웹프로그래밍과제");
+		board.setWriter("양성빈");
 		boardDao.addBoard(board);
 		System.out.println("게시글을 추가했습니다.\n" + board);
 	}
